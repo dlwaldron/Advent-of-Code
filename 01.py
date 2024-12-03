3ae89f4c3e58796f7089b3ed448126c468723158
Daniel <rapidbusiness113@gmail.com>
2024-12-03 17:07:24 +0000

    Update 01.py

--- 01.py
+++ 01.py
@@ -1,26 +1,18 @@
 def part1():
-	left, right, total=[], [], 0
+	left, right=[], []
 	for line in [list(map(int, line.split())) for line in open("01_input.txt", "r").readlines()]:
 		left.append(line[0])
 		right.append(line[1])
-	for i in range(len(left)):
-		total+=abs(min(left) - min(right))
-		left.remove(min(left))
-		right.remove(min(right))
-	print(total)
+	left.sort()
+	right.sort()
+	print("{:^3,}".format(sum([abs(left[val]-right[val]) for val in range(len(left))])))
 
 def part2():
-	lines=open("01_input.txt", "r").readlines()
-	left, right, total, similarity=[], [], 0, 0
-	for line in lines:
-		line=list(map(int, line.split()))
+	left, right=[], []
+	for line in [list(map(int, line.split())) for line in open("01_input.txt", "r").readlines()]:
 		left.append(line[0])
 		right.append(line[1])
-	similarity=sum([num*len([amount for amount in right if amount==num]) for num in left])
-	for i in range(len(left)):
-		total+=abs(min(left) - min(right))
-		left.remove(min(left))
-		right.remove(min(right))
-	print(total, similarity)
+	print("{:^3,}".format(sum([num*len([amount for amount in right if amount==num]) for num in left])))
 
 part1()
+part2()
